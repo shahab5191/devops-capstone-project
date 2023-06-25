269b4ffc00f84140a17a94a2751ec7d7e127cb23
shahab5191 <shahab5191@yahoo.com>
2023-06-25 04:14:42 -0400

    get user route and function

--- service/routes.py
+++ service/routes.py
@@ -68,8 +68,14 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
-
+@app.route("/accounts/<int:id>", methods=["GET"])
+def read_account(id):
+    app.logger.info("Request to read an Account with id: %s", id)
+    account = Account.find(id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{id}] could not be found!")
+    
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
