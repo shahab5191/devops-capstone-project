ee2b25d8c250f445e3ebaca299ba27ef5360f5ca
shahab5191 <shahab5191@yahoo.com>
2023-06-25 05:36:59 -0400

    delete user with id

--- service/routes.py
+++ service/routes.py
@@ -103,8 +103,17 @@ def update_account(id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:id>", methods=["DELETE"])
+def delete_account(id):
+    app.logger.info("Requesting to delete an Account with id: %s", id)
 
+    account = Account.find(id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{id}] could not be found!")
+    
+    account.delete()
+
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
